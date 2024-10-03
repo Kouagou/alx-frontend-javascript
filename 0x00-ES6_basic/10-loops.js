21e6ab01a3f124dbb4da891c92541f354ba4db82
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-10-03 09:02:45 -0700

    Task 10. For...of Loops

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const appendedArray = [];
+  for (const idx of array) {
+    appendedArray.push(appendString + idx);
+  }
+
+  return appendedArray;
+}
