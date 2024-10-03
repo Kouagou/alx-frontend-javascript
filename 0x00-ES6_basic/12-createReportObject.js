9810ea6ead2e254c512b685dc3ffe1d81f366f2e
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-10-03 16:18:18 -0700

    Task 12. Let's create a report object

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,14 @@
+import createEmployeesObject from './11-createEmployeesObject';
+
+export default function createReportObject(employeesList) {
+  const obj = {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    }
+  };
+
+  return obj;
+}
