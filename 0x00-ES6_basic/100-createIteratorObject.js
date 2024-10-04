b7bca54b31dbfddcbb31ea509639ff58ab3611bf
Kouagou Kenneth <ntchakenneth@gmail.com>
2024-10-03 17:07:08 -0700

    Advanced Task 13. Iterating through report objects

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,6 +1,6 @@
 export default function createIteratorObject(report) {
   const employeeList = [];
-  for (const [department, employees] of Object.entries(report.allEmployees)) {
+  for (const [, employees] of Object.entries(report.allEmployees)) {
     for (const employee of employees) {
       employeeList.push(employee);
     }
